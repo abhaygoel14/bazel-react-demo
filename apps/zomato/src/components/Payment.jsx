521d7d55b534320407d5192973b88ac9f5639aa0
Abhay <abhay.goel14@gmil.com>
2026-01-07 19:41:06 +0530

    text change for zomato test

---
+++ apps/zomato/src/components/Payment.jsx
@@ -0,0 +1,25 @@
+const Payment = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <a href="/">Logo</a>
+        </div>
+        <ul className="navbar-menu">
+          <li className="navbar-item">
+            <a href="#payments" className="navbar-link">
+              Payments
+            </a>
+          </li>
+          <li className="navbar-item">
+            <a href="#restaurants" className="navbar-link">
+              Restaurants
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Payment;
